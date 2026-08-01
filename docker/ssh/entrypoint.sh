13538a1b33f23ae77a636b382d36b70097bf5b21
mrmobi2006-source <mrmobi2006@gmail.com>
2026-08-01 19:25:44 +0100

    Update entrypoint.sh to configure DNS
    Add reliable DNS settings to entrypoint script

--- docker/ssh/entrypoint.sh
+++ docker/ssh/entrypoint.sh
@@ -4,6 +4,12 @@ set -e
 SSH_USER="${SSH_USERNAME:?SSH_USERNAME env var required}"
 SSH_PASS="${SSH_PASSWORD:?SSH_PASSWORD env var required}"
 
+# فرض DNS موثوق - بدونه قد يفشل السيرفر بحل أسماء المواقع
+cat > /etc/resolv.conf <<EOF
+nameserver 1.1.1.1
+nameserver 8.8.8.8
+EOF
+
 id -u "$SSH_USER" &>/dev/null || adduser -D -s /bin/bash "$SSH_USER"
 echo "${SSH_USER}:${SSH_PASS}" | chpasswd
 
