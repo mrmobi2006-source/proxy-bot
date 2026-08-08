b8a077129b582f11d055e501775db61c4a73828f
mrmobi2006-source <mrmobi2006@gmail.com>
2026-08-08 04:41:42 +0100

    Update entrypoint.sh

--- docker/xray-shared/entrypoint.sh
+++ docker/xray-shared/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 set -e
-: "${API_SECRET:?API_SECRET env var required}"
+
+: "${API_SECRET:?API_SECRET is required}"
+: "${XRAY_PROTOCOL:?XRAY_PROTOCOL is required}"
+
 exec python3 /app/manage.py
